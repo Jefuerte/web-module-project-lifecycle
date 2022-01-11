10edb8e621691184ffbb2ed937ade0d2cade80e7
jessica <fuerte.jessica10@gmail.com>
2022-01-11 17:34:25 -0600

    did user and followerlisat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import User from "./components/User";
+import FollowerList from "./components/FollowerList";
 
 class App extends React.Component {
+ state = {
+   currentUser: "jefuerte",
+   user: {
+    avatar_url: "https://avatars.githubusercontent.com/u/91096674?v=4",
+    html_url: "https://github.com/Jefuerte",
+    name: "Jessica Fuerte",
+    login: "Jefuerte",
+    public_repos: 32,
+    followers: 2,
+   },
+   followers: [
+     {
+      login: "Cheyenneb96",
+      avatar_url: "https://avatars.githubusercontent.com/u/75545636?v=4",
+      html_url: "https://github.com/Cheyenneb96",
+     },
+     {
+      login: "Cheyenneb96",
+      avatar_url: "https://avatars.githubusercontent.com/u/75545636?v=4",
+      html_url: "https://github.com/Cheyenneb96",
+     }
+   ]
+ }
   render() {
     return(<div>
       <h1>Github Info</h1>
@@ -8,27 +33,9 @@ class App extends React.Component {
         <input placeholder="Username" />
         <button>Search</button>
       </form>
-      <div id="userInfo">
-      <img src="https://avatars.githubusercontent.com/u/91096674?v=4"/>
-      <a target="_blank" href="https://github.com/Jefuerte"><h3>Jessica Fuerte</h3></a>
-      <p>(Jefuerte)</p>
-      <p>Total Repos: 32</p>
-      <p>Total Followers: 2</p>
-      </div>
+      <User user={this.state.user} />
 
-      <div id="followers">
-        <div className="follower">
-          <img width="200px" src="https://avatars.githubusercontent.com/u/85258866?v=4" />
-         <a target="_blank" href="https://github.com/peterdavidconley"> <p>Cheyenneb96</p></a>
-        </div>
-      </div>
-
-      <div id="followers">
-        <div className="follower">
-          <img src="https://avatars.githubusercontent.com/u/75545636?v=4" />
-         <a target="_blank" href="https://github.com/Cheyenneb96"> <p>Cheyenneb96</p></a>
-        </div>
-      </div>
+      <FollowerList followers={this.state.followers} />
     </div>);
   }
 }
